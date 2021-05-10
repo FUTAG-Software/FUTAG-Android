23ffd46e7078741b101b1efa14ae4dd805688e70
Adem ATİCİ <58858983+aticiadem@users.noreply.github.com>
2021-05-10 11:44:31 +0300

    Acilis Ekranlari, gorseller ve renkler eklendi

---
+++ app/src/main/java/com/futag/futag/view/fragment/SplashFragment.kt
@@ -0,0 +1,39 @@
+package com.futag.futag.view.fragment
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.futag.futag.R
+import com.futag.futag.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+    private val BEKLEME_SURESI = 1500L
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_viewPagerFragment)
+        },BEKLEME_SURESI)
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
---
+++ app/src/main/java/com/futag/futag/view/onboarding/screens/FirstScreen.kt
@@ -0,0 +1,32 @@
+package com.futag.futag.view.onboarding.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.futag.futag.databinding.FragmentFirstScreenBinding
+
+class FirstScreen : Fragment() {
+
+    private var _binding: FragmentFirstScreenBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFirstScreenBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
