7492ea01c65a1febef318372ab9920f9ab1c0d72
Adem ATİCİ <58858983+aticiadem@users.noreply.github.com>
2021-05-31 10:52:09 +0300

    Birimlere gecis tamamlandi, proje guncellendi ve buglar giderildi

--- app/src/main/java/com/futag/futag/view/fragment/akis/dahasi/birimlerimiz/BirimlerimizF.kt
+++ app/src/main/java/com/futag/futag/view/fragment/akis/dahasi/birimlerimiz/BirimlerimizF.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.futag.futag.R
 import com.futag.futag.databinding.FragmentBirimlerimizBinding
 
@@ -20,6 +22,41 @@ class BirimlerimizF : Fragment() {
         _binding = FragmentBirimlerimizBinding.inflate(inflater,container,false)
         val view = binding.root
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.birimDisIliskiler.setOnClickListener {
+            val action = BirimlerimizFDirections.actionBirimlerimizFToBirimDetayF(0)
+            Navigation.findNavController(it).navigate(action)
+        }
+
+        binding.birimGirisimcilik.setOnClickListener {
+            val action = BirimlerimizFDirections.actionBirimlerimizFToBirimDetayF(1)
+            Navigation.findNavController(it).navigate(action)
+        }
+
+        binding.birimMedya.setOnClickListener {
+            val action = BirimlerimizFDirections.actionBirimlerimizFToBirimDetayF(2)
+            Navigation.findNavController(it).navigate(action)
+        }
+
+        binding.birimProjeArGe.setOnClickListener {
+            val action = BirimlerimizFDirections.actionBirimlerimizFToBirimDetayF(3)
+            Navigation.findNavController(it).navigate(action)
+        }
+
+        binding.birimSosyalSorumluluk.setOnClickListener {
+            val action = BirimlerimizFDirections.actionBirimlerimizFToBirimDetayF(4)
+            Navigation.findNavController(it).navigate(action)
+        }
+
+        binding.birimYazilim.setOnClickListener {
+            val action = BirimlerimizFDirections.actionBirimlerimizFToBirimDetayF(5)
+            Navigation.findNavController(it).navigate(action)
+        }
+
     }
 
     override fun onDestroyView() {
