3c39e6986508e129213c42dfc3b79f70f8269463
adematici <adematicis41@gmail.com>
2021-08-15 10:20:02 +0300

    colors and layouts updated

--- app/src/main/java/com/futag/futag/model/DahasiItemModel.kt
+++
@@ -1,8 +0,0 @@
-package com.futag.futag.model
-
-import android.graphics.drawable.Drawable
-
-data class DahasiItemModel(
-    val resim: Drawable,
-    val baslik: String
-)
---
+++ app/src/main/java/com/futag/futag/model/MoreoverItemModel.kt
@@ -0,0 +1,8 @@
+package com.futag.futag.model
+
+import android.graphics.drawable.Drawable
+
+data class MoreoverItemModel(
+    val image: Drawable,
+    val title: String
+)
